745af802b66392a04e3f5aaa92dd625a266fccae
rivmos <rivmos.official@gmail.com>
2023-08-28 00:25:22 +0530

    changes

--- frontend/src/services/productServices.js
+++ frontend/src/services/productServices.js
@@ -2,8 +2,8 @@ import axios from "axios";
 const baseUrl = import.meta.env.VITE_APP_BMPM_API
 
 const getDropDownData = async () => {
-    const request = axios.get(`${baseUrl}/dropdowndata`)
-    return request.then(response => {console.log(response.data);return response.data})
+    const request = await axios.get(`${baseUrl}/dropdowndata`)
+    return request.data
 }
 
 const getProduct = async (id) => {
